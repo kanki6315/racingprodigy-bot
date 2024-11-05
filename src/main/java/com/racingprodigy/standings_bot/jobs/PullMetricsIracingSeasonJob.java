cf39dd7da0119cf856c5db1d9f39b2c3e8c3b9e6
Arjuna Kankipati <kanki6315@gmail.com>
2024-11-04 21:48:25 -0800

    Refactor PullMetricsIracingSeasonJob to support multiple series
    Removed hardcoded IDs and refactored job to handle multiple series dynamically. Added functionality to filter metrics based on series type and improved error handling by logging and sending stack traces to a specified channel.

--- src/main/java/com/racingprodigy/standings_bot/jobs/PullMetricsIracingSeasonJob.java
+++ src/main/java/com/racingprodigy/standings_bot/jobs/PullMetricsIracingSeasonJob.java
@@ -4,7 +4,6 @@ import com.racingprodigy.standings_bot.data.RPIracingDriver;
 import com.racingprodigy.standings_bot.data.RPIracingDriverRepository;
 import com.racingprodigy.standings_bot.iracing.IracingService;
 import com.racingprodigy.standings_bot.iracing.model.IracingSeasonStandingChunkResponse;
-import com.racingprodigy.standings_bot.template.TableService;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
@@ -27,15 +26,10 @@ public class PullMetricsIracingSeasonJob {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PullMetricsIracingSeasonJob.class);
 
-    private final static String seasonID = "5029";
-    private final static String carClassID = "74";
-
     @Autowired
     private JDA api;
     @Autowired
     private IracingService iracingService;
-    @Autowired
-    private TableService tableService;
     @Autowired
     private RPIracingDriverRepository rpIracingDriverRepository;
 
@@ -48,7 +42,7 @@ public class PullMetricsIracingSeasonJob {
     @Scheduled(cron = "0 0 2 * * *")
     public void run() {
         try {
-            var rpDrivers = rpIracingDriverRepository.findAll();
+            var rpDrivers = rpIracingDriverRepository.findAllBySeriesTypeEquals(RPIracingDriver.Series.GLOBAL_MAZDA);
             var ids = rpDrivers.stream().map(RPIracingDriver::getId).toList();
 
             if (ids.isEmpty()) {
@@ -56,35 +50,44 @@ public class PullMetricsIracingSeasonJob {
                 return;
             }
 
-            LOGGER.info("Updating iRacing results");
+            LOGGER.info("Pulling iRacing data");
 
-            var seasonStandings = iracingService.getSeriesResults(seasonID, carClassID);
-            List<IracingSeasonStandingChunkResponse> seasonChunk = new ArrayList<>();
-            for (String s : seasonStandings.chunkInfo().chunkFileNames()) {
-                seasonChunk.addAll(iracingService.getChunkSeriesResult(seasonStandings.chunkInfo().baseDownloadUrl() + s));
-            }
-            var filteredDrivers = seasonChunk.stream().filter(d -> ids.contains(d.custId())).toList();
-
-            var seasonInfo = iracingService.getiRacingSeries(seasonID);
-
-            var countUnique = filteredDrivers.size();
-            var mostRacesDone = filteredDrivers.stream().mapToInt(IracingSeasonStandingChunkResponse::starts).max().orElse(0);
-            var totalStarts = filteredDrivers.stream().mapToInt(IracingSeasonStandingChunkResponse::starts).sum();
-            var avgStarts = totalStarts / (countUnique);
-
-            var builder = new EmbedBuilder()
-                    .setTitle("Analytics - " + LocalDate.now().format(DateTimeFormatter.ofPattern("MM/dd/yy")))
-                    .addField("Unique Drivers", Integer.toString(countUnique), true)
-                    .addField("Most Races Done", Integer.toString(mostRacesDone), true)
-                    .addField("Avg Starts per Driver", Integer.toString(avgStarts), true)
-                    .addField("Total Starts", Integer.toString(totalStarts), true).build();
-
-            var channel = api.getTextChannelById(reportingChannelId);
-            channel.sendMessageEmbeds(builder).queue();
+            pullMetricsForSeries(ids, RPIracingDriver.Series.GLOBAL_MAZDA);
+            pullMetricsForSeries(ids, RPIracingDriver.Series.GR86);
         } catch (Exception ex) {
             LOGGER.error("Failed to pull iRacing results", ex);
             var channel = api.getChannelById(TextChannel.class, errorChannelId);
             sendStackTraceToChannel("Failed to pull iRacing results", channel, ex);
         }
+    }
+
+    private void pullMetricsForSeries(List<Integer> ids, RPIracingDriver.Series series) throws Exception {
+        var seasonStandings = iracingService.getSeriesResults(series.getSeasonID(), series.getCarClassID());
+        List<IracingSeasonStandingChunkResponse> seasonChunk = new ArrayList<>();
+        for (String s : seasonStandings.chunkInfo().chunkFileNames()) {
+            seasonChunk.addAll(iracingService.getChunkSeriesResult(seasonStandings.chunkInfo().baseDownloadUrl() + s));
+        }
+
+        List<IracingSeasonStandingChunkResponse> filteredDrivers;
+        if (series.isNeedsFiltering()) {
+            filteredDrivers = seasonChunk.stream().filter(d -> ids.contains(d.custId())).toList();
+        } else {
+            filteredDrivers = seasonChunk;
+        }
+
+        var countUnique = filteredDrivers.size();
+        var mostRacesDone = filteredDrivers.stream().mapToInt(IracingSeasonStandingChunkResponse::starts).max().orElse(0);
+        var totalStarts = filteredDrivers.stream().mapToInt(IracingSeasonStandingChunkResponse::starts).sum();
+        var avgStarts = totalStarts / (countUnique);
+
+        var builder = new EmbedBuilder()
+                .setTitle("Analytics " + series.name() + " - " + LocalDate.now().format(DateTimeFormatter.ofPattern("MM/dd/yy")))
+                .addField("Unique Drivers", Integer.toString(countUnique), true)
+                .addField("Most Races Done", Integer.toString(mostRacesDone), true)
+                .addField("Avg Starts per Driver", Integer.toString(avgStarts), true)
+                .addField("Total Starts", Integer.toString(totalStarts), true).build();
+
+        var channel = api.getTextChannelById(reportingChannelId);
+        channel.sendMessageEmbeds(builder).queue();
     }
 }
